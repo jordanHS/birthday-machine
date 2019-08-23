20c513720352e4551c71be5d12db19afdb30652c
Jordan Safford <jordansafford@gmail.com>
2019-08-23 16:24:39 -0700

    add variables in Javascript

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+
+var phrases = ["Beep!", "Boop!", "I'm sorry, Dave. I'm afraid I can't do that."]
+var number =
+
+
+
+
+
+
+$(document).ready(function() {
+  $("#beep-boop form").submit(function(event) {
+
+event.preventDefault();
+  });
+});
