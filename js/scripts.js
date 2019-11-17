a6ca88d064db693bd0cfe949712482bc5f96542e
Student <example@example.com>
2019-11-17 13:28:32 -0800

    move loop to function & display results

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,38 @@
+ 
  var sentence = function() {
 
         return "Happy Birthday, " + name + "!" ;
       }
 
 
+  
+  function BirthdayCount(number) {
     var count = [];
-
+    console.log(count);
     for (var i = 1; i <= number; i++) {
-
       if (i % 10 === 0) {
         count.push(sentence());
-      }
-
+      } 
       else if (i % 5 === 0) {
         count.push("Birthday!");
-      }
+      } 
       else if (i % 2 !== 0) {
         count.push("Happy");
-      }
+      } 
       else {
         count.push(" " + i);
       }
-
     }
-    console.log(count);
-
+    return count;
+  }
 
 $(document).ready(function() {
   $("#happy-birthday").submit(function(event) {
     event.preventDefault();
     var number = $("input#number").val();
-    var name = $("input#name").val()
-      $(".output").show().text(count);
+    var name = $("#name").val();
+    var count = BirthdayCount(number);
+    $(".output").show().text(count);
     
   });
-
-
 });
